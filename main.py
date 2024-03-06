bae2f81e7f49fb9a13fc4f64021b8fbd00df4605
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-06 16:32:58 -0300

    rota de POST com banco de dados funcionando

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ app.json.sort_keys = False
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
-    
-    cursor = db.cursor()
-    cursor.execute('SELECT * FROM carros')
-    meus_carros = cursor.fetchall()
+    try:
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM carros')
+        meus_carros = cursor.fetchall()
+    except Exception as e:
+        print(f'Error in getting cars: {e}')
     
     carros = []
     for carro in meus_carros:
@@ -34,6 +36,16 @@ def get_carros():
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
+    
+    try:
+        cursor = db.cursor()
+        query = f"INSERT INTO carros (marca, modelo, ano) VALUES ('{carro['marca']}', '{carro['modelo']}', {carro['ano']})"
+        cursor.execute(query)
+        db.commit()
+    except Exception as e:
+        print(f'Erro in posting car: {e}')
+        return
+
     return make_response(jsonify(
         message='Carro cadastrado com sucesso',
         carro=carro
