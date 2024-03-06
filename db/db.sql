c74dfc3b1f736bc31f682ee1249611e07edf0488
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-06 15:44:06 -0300

    API adaptada para utilizar um banco de dados real, rota GET finalizada

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE carros (
+    id INTEGER PRIMARY KET AUTO_INCREMENT,
+    marca, VARCHAR(100),
+    modelo VARCHAR(100),
+    ano INTEGER
+);
+
+INSERT INTO carros (marca, modelo, ano) VALUES ('Fiat', 'Marea', 1999);
+INSERT INTO carros (marca, modelo, ano) VALUES ('Toyota', 'Civic', 2023);
+INSERT INTO carros (marca, modelo, ano) VALUES ('Honda', 'Corolla', 2022);
+INSERT INTO carros (marca, modelo, ano) VALUES ('Chevrolet', 'Celta', 2003);
+INSERT INTO carros (marca, modelo, ano) VALUES ('Fiat', 'Uno', 2005);
+
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 from flask import make_response, jsonify, request, Flask
-from bd import carros
+import mysql.connector
 
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='test'
+)
 app = Flask(__name__)
+app.json.sort_keys = False
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
+    
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM carros')
+    meus_carros = cursor.fetchall()
+    
+    carros = []
+    for carro in meus_carros:
+        carros.append({
+            'id': carro[0],
+            'marca': carro[1],
+            'modelo': carro[2],
+            'ano': carro[3]
+        })
+
     return make_response(jsonify(
         message='Lista de carros',
-        carros=carros
+        carros=carros       
     ))
 
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
-    print(carro)
     return make_response(jsonify(
         message='Carro cadastrado com sucesso',
         carro=carro
     ))
 
-#Rodar server
 app.run()
